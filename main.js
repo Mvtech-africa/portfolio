762b65450314c13d83669a99bffc2af135ac7d05
mvtech <mvtech44@gmail.com>
2025-06-13 18:16:47 +0100

    new commit

--- main.js
+++ main.js
@@ -26,3 +26,63 @@ function toggleMenu () {
 
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//  mobile screen carousel
+
+
+
+ const images = [
+    'b4.jpg',
+    'b4.jpg',
+    'b4.jpg',
+    'b4.jpg',
+    'b4.jpg'
+  ];
+
+  const track = document.getElementById('track');
+  const slideWidth = 310; // 270 + 2*20 margin
+  let position = 0;
+  const slides = [];
+
+  // Create slides
+  images.forEach((src) => {
+    const slide = document.createElement('div');
+    slide.className = 'slide';
+    const img = document.createElement('img');
+    img.src = src;
+    slide.appendChild(img);
+    track.appendChild(slide);
+    slides.push(slide);
+  });
+
+  function updateClasses() {
+    slides.forEach(slide => slide.className = 'slide');
+    if (slides[position]) slides[position].classList.add('slide', 'active');
+    if (slides[position - 1]) slides[position - 1].classList.add('slide', 'prev');
+    if (slides[position + 1]) slides[position + 1].classList.add('slide', 'next');
+  }
+
+  function updatePosition() {
+    track.style.transform = `translateX(calc(50% - ${position * slideWidth + slideWidth / 2}px))`;
+    updateClasses();
+  }
+
+  function nextSlide() {
+    position = (position + 1) % images.length;
+    updatePosition();
+  }
+
+  let autoSlide = setInterval(nextSlide, 3000);
+
+  updatePosition();
